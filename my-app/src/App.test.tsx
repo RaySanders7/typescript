763d6a333fdec9b65c2adfd211bc28d30c22f082
Ray <raymond.sanders@gmail.com>
2018-02-01 14:26:43 -0600

    WIP

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as TestUtils  from 'react-dom/test-utils';
+import * as ShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import App from './App';
+import StatefulComponent from './StatefulComponent';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  let renderer: ShallowRenderer;
+
+  beforeEach(() => {
+    renderer =  TestUtils.createRenderer();
+    renderer.render(<App />);
+  });
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+  
+  it('renders a component that will changed based on state', () => {
+    const wrapper = renderer.getRenderOutput();
+    wrapper.find(<StatefulComponent />);
+  });
 });
---
+++ my-app/src/StatefulComponent.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class StatefulComponent extends React.Component {
+  render() {
+    return (
+      <div className="StatefulComponent">
+        Chello
+      </div>
+    );
+  }
+}
+
+export default StatefulComponent;
